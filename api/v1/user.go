17a2518fc2c5f58d93268e8452a5b01ec23255e1
dewasatriawan <dewa@bsa.id>
2023-06-01 23:15:12 +0800

    update send verification email

--- api/v1/user.go
+++ api/v1/user.go
@@ -137,7 +137,7 @@ func (h userHandler) Create(c *gin.Context) {
 
 		go func(user model.User) {
 			config := h.infra.Config().Sub("server")
-			urlActivation := fmt.Sprintf("%s:%s/v1/auth/activation?token=%s", config.GetString("url"), config.GetString("port"), activationData.Token)
+			urlActivation := fmt.Sprintf("%s/v1/auth/activation?token=%s", config.GetString("web_url"), activationData.Token)
 
 			if err := email.New(h.infra.GoMail(), h.infra.Config()).SendActivation(user.FirstName, user.Email, urlActivation); err != nil {
 				log.Printf("Error Send Email E: %v", err)
